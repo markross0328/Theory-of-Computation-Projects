ca8f9a34edf440584ba441ef6888b6bef8eba6ad
Mark Ross <121188485+markross0328@users.noreply.github.com>
2025-04-11 18:33:17 -0400

    Update main.cpp

--- Program 1/main.cpp
+++ Program 1/main.cpp
@@ -62,11 +62,8 @@ bool is_valid_literal(const std::string &literal)
 
 void trim(std::string &s)
 {
-    s.erase(s.begin(), std::find_if(s.begin(), s.end(), 
-        [](unsigned char c) { return !std::isspace(c); }));
-    s.erase(std::find_if(s.rbegin(), s.rend(), 
-        [](unsigned char c) { return !std::isspace(c); }).base(),
-            s.end());
+    s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char c) { return !std::isspace(c); }));
+    s.erase(std::find_if(s.rbegin(), s.rend(), [](unsigned char c) { return !std::isspace(c); }).base(), s.end());
 }
 
 int main(int argc, char *argv[])
